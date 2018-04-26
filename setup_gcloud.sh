8bb93dc1b2ec2b1d61478ebb61684277f0f96163
Vishnu <psvishnu.91@gmail.com>
2018-04-26 02:26:18 +0000

    Something

--- setup_gcloud.sh
+++ setup_gcloud.sh
@@ -7,5 +7,11 @@ sudo apt-get update
 sudo apt-get -y install python3.6
 virtualenv  --python=/usr/bin/python3.6 venv3
 source venv3/bin/activate
+sudo apt-get update && sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
 
+
+#git clone https://github.com/psvishnu91/sonic.git
+#cd sonic
+#git clone https://github.com/openai/baselines.git
+#pip3 install -e ./baselines
 
